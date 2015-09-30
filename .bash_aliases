213490ec6257b40bec83e10a09efaa0555d9b26c
Masahiro <gigaelectronvolts@gamil.com>
2015-10-01 06:30:13 +0900

    [modify] Change the way to start 256 colors tmux
    Add startup option by using alias

---
+++ .bash_aliases
@@ -0,0 +1,4 @@
+# Alias definitions.
+
+# Force tmux to assume the terminal supports 256 colors.
+alias tmux='tmux -2'
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/colors ~/.vim
